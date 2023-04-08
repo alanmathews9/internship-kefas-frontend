91a46c780869456daf638bda2a459d9041b0a7a4
alan <alangeorgemathews09@gmail.com>
2023-04-08 18:18:22 +0530

    Deleted comments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 class navbar extends Component {
+
+
+
+
+  
   render() {
     return (
       <div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -6,10 +6,6 @@ import Navbar from "./Navbar";
 class Signup extends Component {
   constructor(props) {
     super(props);
-    // this.setName = this.setName.bind(this);
-    // this.setMail = this.setMail.bind(this);
-    // this.setPassword = this.setPassword.bind(this);
-    // this.handleSignIn = this.handleSignIn.bind(this);
     this.state = {
       signinInfo: {
         name: '',
@@ -27,32 +23,9 @@ class Signup extends Component {
     console.log(this.state.signinInfo.email_id);
     console.log(this.state.signinInfo.password);
   };
-  // setName(e){
-  //   this.setState({
-  //     name: e.target.value
-  //   });
-    
-  // };
-  
-  // setMail(e){
-  //   this.setState({
-  //     email: e.target.value
-  //   });
-  // };
-  
-  // setPassword(e){
-  //   this.setState({
-  //     password: e.target.value
-  //   });
-    
-  // };
     
 handleSignUp = (e) =>{
   e.preventDefault();
-    // const formData = new FormData();
-    // formData.append("name", this.state.name);
-    // formData.append("email_id", this.state.email);
-    // formData.append("password", this.state.password);
   axios.post("http://127.0.0.1:8000/register_user/", this.state.signinInfo)
     .then((response) => { 
       if (response.data.status === "success") {
@@ -67,7 +40,6 @@ handleSignUp = (e) =>{
     }).catch((error) => {
         console.log(error);
       });
-  // this.setState({name: '',email: '', password: ''})
   };
   
   render() {
@@ -81,8 +53,7 @@ handleSignUp = (e) =>{
                 Name:
                 <input
                   name="name"
-                  type="text"
-                  // value={this.state.name}           
+                  type="text"     
                   onChange={this.handleChange}
                   required
                 />
@@ -92,7 +63,6 @@ handleSignUp = (e) =>{
                 <input
                   name="email_id"
                   type="email"
-                  // value={this.state.email}
                   onChange={this.handleChange}
                   required
                 />
@@ -102,7 +72,6 @@ handleSignUp = (e) =>{
                 <input
                   type="password"
                   name="password"
-                  // value={this.state.password}
                   onChange={this.handleChange}
                   required
                 />
