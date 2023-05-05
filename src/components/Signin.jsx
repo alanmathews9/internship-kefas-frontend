38f995acf113422c95f4a1e9b33dd16608389ae7
alan <alangeorgemathews09@gmail.com>
2023-05-05 19:53:34 +0530

    Changed ui

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -88,7 +88,7 @@ class Signin extends Component {
               <button type="submit" className="bg-dark">Sign In</button>
             </form>
             <p>
-              Don't have an account? <a href="/sign-up" className= "link-change btn btn-sm bg-dark btn-link">Sign Up</a>
+              Don't have an account? <a href="/sign-up">Sign Up</a>
             </p>
           </div>
         </div>
