7a5e30348beb0fac45fc4d4924071ce0838582c3
Alan <alangeorgemathews09@gmail.com>
2023-09-07 19:57:55 +0530

    Added comments

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -6,40 +6,40 @@ class Signin extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loginInfo: {
+      loginInfo: {      // loginInfo state to store email id and password
         email_id: '',
         password: '',
       },
-      errorMessage: {
+      errorMessage: {   // errorMessage state to store error message
         email_id: '',
         password: '',
       },
     };
   }
   
-  handleChange = (e) => {
-    let loginInfo  = { ...this.state.loginInfo };
-    loginInfo[e.target.name] = e.target.value;
-    this.setState({ loginInfo });
-    console.log(this.state.loginInfo.email_id);
+  handleChange = (e) => {                           // function to handle change in input fields
+    let loginInfo  = { ...this.state.loginInfo };   // loginInfo state is copied to loginInfo variable
+    loginInfo[e.target.name] = e.target.value;      // value of input field is stored in loginInfo state
+    this.setState({ loginInfo });                   // loginInfo state is set to loginInfo variable
+    console.log(this.state.loginInfo.email_id);     
     console.log(this.state.loginInfo.password);
   };
     
-  handleSignIn = (e) => {
+  handleSignIn = (e) => {                         // function to handle sign in
     e.preventDefault();
     console.log(this.state.loginInfo);
-    axios.post("http://127.0.0.1:8000/login/", this.state.loginInfo )
+    axios.post("http://127.0.0.1:8000/login/", this.state.loginInfo ) // email id and password is passed to login
       .then((response) => {
         if (response.data.status === "success") {
-          localStorage.setItem("session_id", response.data.session_id);
+          localStorage.setItem("session_id", response.data.session_id); // session id is stored in local storage
           localStorage.setItem("email_id", this.state.loginInfo.email_id);
           window.location.href="/home"
         }
         else if (response.data.status === "failure") {
-          let errorMessage = { ...this.state.errorMessage };
+          let errorMessage = { ...this.state.errorMessage };  // errorMessage state is copied to errorMessage variable
           errorMessage.email_id = 'Incorrect email id or password';
           errorMessage.password = 'Incorrect email id or password';
-          this.setState({ errorMessage });
+          this.setState({ errorMessage });                    // errorMessage state is set to errorMessage variable
         }
       }).catch((error) => {
         console.log(error);
@@ -48,7 +48,7 @@ class Signin extends Component {
 
   
   render() {
-    if (localStorage.getItem("session_id")) {
+    if (localStorage.getItem("session_id")) {   // if session id present, logged in user is redirected to home page
       window.location.href = "/home";
     }
     else {
@@ -56,7 +56,7 @@ class Signin extends Component {
         <div>
           <div className="container">
             <h2>Sign In</h2>
-            <form method="POST" onSubmit={this.handleSignIn}>
+            <form method="POST" onSubmit={this.handleSignIn}>  
               <label>
                 <div className="text-place">Email</div>
                 <input
@@ -64,10 +64,10 @@ class Signin extends Component {
                   name="email_id"
                   type="email"
                   placeholder="Email"
-                  onChange={this.handleChange}
+                  onChange={this.handleChange}  // function to handle change in input fields
                   required
                   style={{
-                    borderColor: this.state.errorMessage.email_id ? "red" : "",
+                    borderColor: this.state.errorMessage.email_id ? "red" : "",    // if error message is present, border color is set to red 
                   }}
                 />
 
